7190d2b8e5ccbdf3a63cebcdb9bc62135379acaf
zhangkaifeng <1099503460@qq.com>
2025-07-23 15:26:56 +0800

    add business development

--- sky-server/src/main/java/com/sky/service/SetmealService.java
+++ sky-server/src/main/java/com/sky/service/SetmealService.java
@@ -3,7 +3,9 @@ package com.sky.service;
 
 import com.sky.dto.SetmealDTO;
 import com.sky.dto.SetmealPageQueryDTO;
+import com.sky.entity.Setmeal;
 import com.sky.result.PageResult;
+import com.sky.vo.DishItemVO;
 import com.sky.vo.SetmealVO;
 
 import java.util.List;
@@ -21,4 +23,8 @@ public interface SetmealService {
     void startOrStop(Integer status, Long id);
 
     SetmealVO getByIdWithDish(Long id);
+
+    List<DishItemVO> getDishItemById(Long id);
+
+    List<Setmeal> list(Setmeal setmeal);
 }
---
+++ sky-server/src/main/java/com/sky/service/impl/UserServiceImpl.java
@@ -0,0 +1,75 @@
+package com.sky.service.impl;
+
+import com.alibaba.fastjson.JSONObject;
+import com.sky.constant.MessageConstant;
+import com.sky.dto.UserLoginDTO;
+import com.sky.entity.User;
+import com.sky.exception.LoginFailedException;
+import com.sky.mapper.UserMapper;
+import com.sky.properties.WeChatProperties;
+import com.sky.service.UserService;
+import com.sky.utils.HttpClientUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+@Slf4j
+public class UserServiceImpl implements UserService {
+
+    public static final String WX_LOGIN = "https://api.weixin.qq.com/sns/jscode2session";
+
+    @Autowired
+    private WeChatProperties weChatProperties;
+
+    @Autowired
+    private UserMapper userMapper;
+
+    /**
+     * 微信登录
+     * @param userloginDTO
+     * @return
+     */
+    @Override
+    public User wxLogin(UserLoginDTO userloginDTO) {
+
+        String openid = getOpenid(userloginDTO.getCode());
+
+        //判断openid是否存在，如果为空，则说明登录失败
+        if(openid == null){
+            throw new LoginFailedException(MessageConstant.LOGIN_FAILED);
+        }
+
+        //判断当前用户是否为新用户
+        User user = userMapper.getByOpenid(openid);
+
+        //如果是，则自动完成注册
+        if(user == null){
+            user = User.builder()
+                    .openid(openid)
+                    .createTime(LocalDateTime.now())
+                    .build();
+            userMapper.insert(user);
+        }
+        return user;
+    }
+
+    private String getOpenid(String code) {
+        //调用微信接口，获取openid
+        Map<String, String> map = new HashMap<>();
+        map.put("appid", weChatProperties.getAppid());
+        map.put("secret", weChatProperties.getSecret());
+        map.put("js_code", code);
+        map.put("grant_type", "authorization_code");
+        String json = HttpClientUtil.doGet(WX_LOGIN, map);
+
+        JSONObject jsonObject = JSONObject.parseObject(json);
+        String openid = jsonObject.getString("openid");
+        return openid;
+    }
+
+}
